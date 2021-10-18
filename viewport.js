7af4bc3be6c60a363a4ba0a075c63aad48796c0d
Tobias Buschor <tobias.buschor@shwups.ch>
2021-10-18 14:35:17 +0200

    Create viewport.js

---
+++ viewport.js
@@ -0,0 +1,32 @@
+
+const docEl = document.documentElement
+let lastY = 0;
+const onScroll = function(){
+    const upScroll = lastY >= pageYOffset;
+    docEl.classList[upScroll?'add':'remove']('u1-scroll-up');
+    lastY = pageYOffset;
+    const isTop = lastY === 0;
+    docEl.classList[isTop?'add':'remove']('u1-scroll-top');
+    if (isTop) {
+        docEl.classList.add('js-up-scroll');
+    }
+};
+onScroll();
+addEventListener('scroll',onScroll);
+
+
+
+
+
+
+let lastHeight = null;
+function check(){
+    let height = window.innerHeight;
+    if (lastHeight !== null) {
+        height = Math.abs(height - lastHeight) > 60 ? height : lastHeight; // grösser oder kleiner geworden um mehr als 60 px
+    }
+    lastHeight = height;
+    document.documentElement.style.setProperty('--u1-vh-max', height+'px');
+};
+check();
+addEventListener('resize',check);
