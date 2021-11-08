14f7fca4df890eda8efe0b3fa2e6a04f0df27573
Tobias Buschor <tobias.buschor@shwups.ch>
2021-11-08 17:55:06 +0100

    Update viewport.js

--- viewport.js
+++ viewport.js
@@ -6,7 +6,7 @@ const onScroll = function(){
     docEl.classList[upScroll?'add':'remove']('u1-scroll-up');
     lastY = pageYOffset;
     //const isTop = lastY === 0;
-    const isTop = lastY <= 0;
+    const isTop = lastY <= 10; // 0 is not enough on iOS
     //docEl.classList[isTop?'add':'remove']('u1-scroll-top');
     docEl.classList[isTop?'remove':'add']('u1-scroll-not-top');
 };
@@ -20,7 +20,7 @@ let lastHMin = null;
 function check(){
     let hMin = window.innerHeight;
     if (lastHMin !== null) {
-        hMin = Math.abs(hMin - lastHMin) > 70 ? hMin : lastHMin; // grösser oder kleiner geworden um mehr als 60 px
+        hMin = Math.abs(hMin - lastHMin) > 150 ? hMin : lastHMin; // tested: 114px
     }
     document.documentElement.style.setProperty('--u1-vh-min', (hMin * 0.01) +'px');
     lastHMin = hMin;
