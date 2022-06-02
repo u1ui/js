cf534f556a22ecf097fe83405d9765fd6091301a
Tobias Buschor <tobias.buschor@shwups.ch>
2022-06-02 13:21:16 +0200

    Update viewport.js

--- viewport.js
+++ viewport.js
@@ -25,3 +25,40 @@ function check(){
 };
 check();
 addEventListener('resize',check);
+
+
+
+
+
+/*
+// https://github.com/joppuyo/large-small-dynamic-viewport-units-polyfill/blob/master/src/index.js
+const docEl = document.documentElement;
+
+if (CSS.supports('height:1svh')) {
+    docEl.style.setProperty('--1svh', "1svh");
+    docEl.style.setProperty('--1dvh', "1dvh");
+    docEl.style.setProperty('--1lvh', "1lvh");
+} else {
+    recalc();
+    document.addEventListener('DOMContentLoaded', recalc)
+    window.addEventListener('resize', recalc);
+}
+
+function recalc() {
+    const svh = docEl.clientHeight * 0.01;
+    docEl.style.setProperty('--1svh', (svh + "px"));
+    const dvh = window.innerHeight * 0.01;
+    docEl.style.setProperty('--1dvh', (dvh + "px"));
+
+    //const parent = docEl;
+    const parent = document.body;
+    if (parent) {
+        const fixed = document.createElement("div");
+        fixed.style.cssText = 'width:1px;height:100vh;position:fixed;left:0;top:0;bottom:0;visibility:hidden';
+        parent.append(fixed);
+        const lvh = fixed.clientHeight * 0.01;
+        docEl.style.setProperty('--1lvh', (lvh + "px"));
+        fixed.remove();
+    }
+};
+*/
